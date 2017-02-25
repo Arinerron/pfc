4b595dea52a72eca9cf28a4f2b78e1430c2cddcc
Arinerron <me@arinerron.com>
2017-02-25 11:11:00 -0800

    Fix bug with modules

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 
 cd src
 
-javac -d ../bin -cp .:$(echo ../lib/*.jar | tr ' ' ':'):$(echo */*.java | tr ' ' ':') Main.java;
+javac -d ../bin -cp .:modules/*.java:modules/:modules/*:$(echo ../lib/*.jar | tr ' ' ':'):$(echo */*.java | tr ' ' ':') Main.java $(echo modules/*);
 
 cd ../bin
 
